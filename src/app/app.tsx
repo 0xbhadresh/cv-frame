06c15a816732981c1c55d692cd3de568e59bc45d
0xbhadresh <bhadresh.lamprostech@gmail.com>
2025-01-17 16:19:13 +0530

    initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import "./globals.css";
+
+const Demo = dynamic(() => import("../components/Demo"), {
+  ssr: false,
+});
+
+export default function App(
+  { title }: { title?: string } = { title: "Frames v2 Demo" }
+) {
+  return <Demo title={title} />;
+}
